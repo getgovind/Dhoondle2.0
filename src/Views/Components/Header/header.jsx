a613f89b9e92662de13df641571c5f310657a670
getupgovind <projectgovind007@gmail.com>
2023-09-14 11:52:20 -0700

    add change by govind

--- src/Views/Components/Header/header.jsx
+++ src/Views/Components/Header/header.jsx
@@ -7,7 +7,7 @@ const Header = () =>{
             <nav class="navbar bg-body-tertiary">
   <div class="container">
     <div className="header_outer_cont">
-    <a class="navbar-brand">Dhoondle</a>
+    <a class="navbar-brand"><img src="Assets\Images\header\WhatsApp Image 2023-09-10 at 12.30.14 PM (1).jpeg" alt="" className="img-fluid" style={{ width :"100%", maxWidth:"100px" }} /></a>
 
     <div className="d-flex gap-3 pt-2 pe-3 ">
     <button className="header_btn" >
